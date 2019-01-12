2be31ac870a81281fec7f871758c23ab87a2673f
gufuu <ntkbusy0213@gmail.com>
2019-01-13 05:16:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/mediasamplekotlin/MediaSampleKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.example.mediasamplekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MediaSampleKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MediaSampleKotlinApplication>(*args)
+}
+
